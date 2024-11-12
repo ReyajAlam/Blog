071a0b543fb856c76bdc50de54b01f8e5b372a86
ReyajAlam <reyajalam0425@gmail.com>
2024-11-12 18:37:16 +0530

    change some elemntat and other

--- client/src/constants/Config.jsx
+++ client/src/constants/Config.jsx
@@ -1,41 +1,25 @@
-// API NOTIFICATION MESSAGES
-export const API_NOTIFICATION_MESSAGES = {
-    loading: {
-        title: "Loading...",
-        message: "Data is being loaded. Please wait"
+const API_NOTIFICATION_MESSAGES = {
+    loading:{
+        title:'loading',
+        message:'Data is being loading, please wait'
     },
-    success: {
-        title: "Success",
-        message: "Data successfully loaded"
+    success:{
+        title:'success',
+        message:'Data SUcessfully Loaded'
     },
-    requestFailure: {
-        title: "Error!",
-        message: "An error occur while parsing request data"
+    responseFailure:{
+        title:'Error',
+        message:'an error occured while fetching response from the server. plese try again'
     },
-    responseFailure: {
-        title: "Error!",
-        message: "An error occur while fetching response from server. Please try again"
+    requestFailure:{
+        title:'Error',
+        message:'an error occured while parsing request data'
     },
-    networkError: {
-        title: "Error!",
-        message: "Unable to connect to the server. Please check internet connectivity and try again."
+    networkError:{
+        title:'Error',
+        message:'unable to connect to server, please check internet connectivity'
     }
 }
-
-// API SERVICE URL
-// SAMPLE REQUEST
-// NEED SERVICE CALL: { url: "/", method: "POST/GET/PUT/DELETE" }
 export const SERVICE_URLS = {
-    userLogin: { url: '/login', method: 'POST' },
-    userSignup: { url: '/signup', method: 'POST' },
-    getAllPosts: { url: '/posts', method: 'GET', params: true },
-    getRefreshToken: { url: '/token', method: 'POST' },
-    uploadFile: { url: 'file/upload', method: 'POST' },
-    createPost: { url: 'create', method: 'POST' },
-    deletePost: { url: 'delete', method: 'DELETE', query: true },
-    getPostById: { url: 'post', method: 'GET', query: true },
-    newComment: { url: '/comment/new', method: 'POST' },
-    getAllComments: { url: 'comments', method: 'GET', query: true },
-    deleteComment: { url: 'comment/delete', method: 'DELETE', query: true },
-    updatePost: { url: 'update', method: 'PUT', query: true }
+    userSignup: { url:'/signup', method:'POST'}
 }
